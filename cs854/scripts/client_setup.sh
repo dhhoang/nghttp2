3d9f6e8b29ab72a6b7c4da8f0692771bc73d170e
Unknown <goaty1992@outlook.com>
2017-11-16 14:14:44 -0500

    fix dep 14

--- cs854/scripts/client_setup.sh
+++ cs854/scripts/client_setup.sh
@@ -1,7 +1,5 @@
 sudo apt-get update
-apt-get install g++ make binutils autoconf automake autotools-dev libtool pkg-config \ 
-zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev libjansson-dev \ 
-libc-ares-dev libjemalloc-dev cython python3-dev python-setuptools -qy;
+apt-get install g++ make binutils autoconf automake autotools-dev libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev libjansson-dev libc-ares-dev libjemalloc-dev cython python3-dev python-setuptools -qy;
 echo "entering dir nghttp2/";
 cd ../../;
 
