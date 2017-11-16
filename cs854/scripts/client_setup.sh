5837b3cc2ba2d798995ccfc6bdad0e337d632074
Unknown <goaty1992@outlook.com>
2017-11-16 14:10:08 -0500

    fix build steps for ubuntu 14

--- cs854/scripts/client_setup.sh
+++ cs854/scripts/client_setup.sh
@@ -1,14 +1,16 @@
 sudo apt-get update
-sudo apt-get install g++ make binutils autoconf automake autotools-dev libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev libjansson-dev libc-ares-dev libjemalloc-dev libsystemd-dev libsystemd-daemon-dev libspdylay-dev cython python3-dev python-setuptools
-sudo apt-get install g++ make binutils autoconf automake autotools-dev libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev -y
-echo "entering dir nghttp2/"
-cd ../../
+apt-get install g++ make binutils autoconf automake autotools-dev libtool pkg-config \ 
+zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev libjansson-dev \ 
+libc-ares-dev libjemalloc-dev cython python3-dev python-setuptools -qy;
+echo "entering dir nghttp2/";
+cd ../../;
 
 function build_nghttp2(){
-    git submodule update --init
-    autoreconf -i && automake && autoconf
-    ./configure --enable-app
-    make
+    autoreconf -i;
+    automake;
+    autoconf;
+    ./configure --enable-apps;
+    make;
 }
 
-build_nghttp2
+build_nghttp2;
