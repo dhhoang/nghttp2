5c2ecfad8619276dc3083a05d5adaf77b91d55ce
Unknown <goaty1992@outlook.com>
2017-11-16 13:41:55 -0500

    add build options for ubuntu 14.04

--- cs854/scripts/client_setup.sh
+++ cs854/scripts/client_setup.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
-sudo apt-get install g++ make binutils autoconf automake autotools-dev libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev libjansson-dev libc-ares-dev libjemalloc-dev libsystemd-dev libspdylay-dev cython python3-dev python-setuptools
+sudo apt-get install g++ make binutils autoconf automake autotools-dev libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev libjansson-dev libc-ares-dev libjemalloc-dev libsystemd-dev libsystemd-daemon-dev libspdylay-dev cython python3-dev python-setuptools
+sudo apt-get install g++ make binutils autoconf automake autotools-dev libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev -y
 echo "entering dir nghttp2/"
 cd ../../
 
